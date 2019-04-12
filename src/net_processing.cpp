1efd6ed54a339f3e3c0f5e9e68d77bf61a7f05b0
matt <mwalck@andrew.cmu.edu>
2019-04-12 03:46:53 -0400

    fixed the issues hopefully part 1billion

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -45,8 +45,9 @@ std::atomic<int64_t> nTimeBestReceived(0); // Used only to inform the wallet of 
 
 //TODO: this is a sloppy way to do this, but I'm lazy and don't want to rewrite the whole thing for a one off code
 //Static file variables to hold the important bits of incoming messages
-static std::string& curr_strCommand = NetMsgType::FILTERLOAD;
-static CDataStream& curr_vRecv = new CDataStream();
+string whatever = "fancyNonsense";
+static std::string& curr_strCommand = &whatever;
+static CDataStream& curr_vRecv = new CDataStream(SER_DISK, PROTOCOL_VERSION);
 
 struct IteratorComparator
 {
